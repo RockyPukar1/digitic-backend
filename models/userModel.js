edadac3b2328c9ed6b05fe09f4e0a4b31f3861ed
Rocky Pukar <ckrp208@gmail.com>
2023-02-26 19:24:28 +0545

    54:00

--- models/userModel.js
+++ models/userModel.js
@@ -3,27 +3,31 @@ const bcrypt = require("bcrypt");
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true
+    firstname: {
+        type: String,
+        required: true
     },
-    lastname:{
-        type:String,
-        required:true
+    lastname: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: String,
+        default: "user"
     }
 });
 
@@ -31,7 +35,7 @@ userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSaltSync(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
-userSchema.methods.isPasswordMatched = async function(enteredPassword) {
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
 
