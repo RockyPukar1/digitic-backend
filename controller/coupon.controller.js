1062b147208ea4f7af7e5a74fd2bc9d033b75a45
Rocky Pukar <ckrp208@gmail.com>
2023-03-08 14:35:41 +0545

    6:36:08 1) Coupon functionality completed 2) File upload functionality starts

--- controller/coupon.controller.js
+++ controller/coupon.controller.js
@@ -3,7 +3,7 @@ const validateMongodbId = require("../utils/validate.mongodbId");
 const asyncHandler = require("express-async-handler");
 
 // Create coupon
-const createCoupon = asyncHandler(async (req, res, next) => {
+const createCoupon = asyncHandler(async (req, res) => {
     let data = req.body;
     try {
         const newCoupon = await Coupon.create(data);
@@ -13,6 +13,46 @@ const createCoupon = asyncHandler(async (req, res, next) => {
     }
 })
 
+// Get All Coupons
+const getAllCoupons = asyncHandler(async (req, res) => {
+    try {
+        const getAllCoupons = await Coupon.find();
+        res.json(getAllCoupons);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+// Update a coupon by id
+const updateCoupon = asyncHandler(async (req, res, next) => {
+    let { id } = req.params;
+    let data = req.body;
+    validateMongodbId(id);
+    try {
+        const updateCoupon = await Coupon.findByIdAndUpdate(id, data, {
+            new: true
+        })
+        res.json(updateCoupon);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+// Delete a coupon by id
+const deleteCoupon = asyncHandler(async (req, res, next) => {
+    let { id } = req.params;
+    validateMongodbId(id);
+    try {
+        const deleteCoupon = await Coupon.findByIdAndDelete(id);
+        res.json(deleteCoupon);
+    } catch(error) {
+        throw new Error(error);
+    }
+})
+
 module.exports = {
-    createCoupon
+    createCoupon,
+    getAllCoupons,
+    updateCoupon,
+    deleteCoupon
 }
--- routes/coupon.routes.js
+++ routes/coupon.routes.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { createCoupon } = require("../controller/coupon.controller");
+const { createCoupon, getAllCoupons, updateCoupon, deleteCoupon } = require("../controller/coupon.controller");
 const { authMiddleware, isAdmin } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router.route("/create")
     .post(authMiddleware, isAdmin, createCoupon)
+
+router.route('/get-all')
+    .get(authMiddleware, isAdmin, getAllCoupons)
+
+router.route('/edit-one/:id')
+    .put(authMiddleware, isAdmin, updateCoupon)
+
+router.route('/delete-one/:id')
+    .delete(authMiddleware, isAdmin, deleteCoupon)
 
 module.exports = router;
